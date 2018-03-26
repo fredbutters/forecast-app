4358ac969c447a78ccd60d62f87e6a405def3c77
Mark Barry <mark.barry@iherb.net>
2018-03-26 11:33:23 -0700

    using immer

--- src/containers/Users/reducer.js
+++ src/containers/Users/reducer.js
@@ -1,19 +1,32 @@
-import immer from "immer";
+import produce from "immer";
 
 let initialState = { users: [], selectedUser: {} };
-export const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "LOAD":
-            return {
-                ...state,
-                users: action.payload.response.results
-            };
-        case "SELECT_USER":
-            return {
-                ...state,
-                selectedUser: action.payload
-            };
-        default:
-            return state;
-    }
-};
+export const reducer = (state = initialState, action) =>
+    produce(state, draft => {
+        switch (action.type) {
+            case "LOAD":
+                draft.users = action.payload.response.results;
+                break;
+            case "SELECT_USER":
+                draft.selectedUser = action.payload;
+                break;
+        }
+        return draft;
+    });
+
+// export const reducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case "LOAD":
+//             return {
+//                 ...state,
+//                 users: action.payload.response.results
+//             };
+//         case "SELECT_USER":
+//             return {
+//                 ...state,
+//                 selectedUser: action.payload
+//             };
+//         default:
+//             return state;
+//     }
+// };
